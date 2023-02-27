a4ac0a3a8a60a00a3c2a0a91b9a7e79742f28c7e
Brennan Hatton <brennan@brennanhatton.com>
2023-02-27 12:17:17 +1100

    Making road generation code

---
+++ Assets/BrennanHatton/Tiling System/TilePlacer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePlacer : MonoBehaviour
+{
+	public Tile tilePrefab;
+	public List<Tile> tilesPlaced;
+	public Transform origin;
+	public Transform next;
+	public float number = 10;
+	
+	void Reset()
+	{
+		origin = this.transform;
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+	{
+		Setup();
+	}
+    
+	public void Setup()
+	{
+		next.transform.position = origin.position;
+		next.transform.rotation = origin.rotation;
+	}
+    
+	public void PlaceTiles(float numberOfTiles)
+	{
+		for(int i = 0; i < numberOfTiles; i++)
+			PlaceNexTile();
+	}
+	
+	public void PlaceNexTile()
+	{
+		Tile tile = Instantiate<Tile>(tilePrefab, next.position, next.rotation, origin);
+		
+		tilesPlaced.Add(tile);
+		
+		next.transform.position += next.transform.forward*tile.length;
+	
+	}
+}
