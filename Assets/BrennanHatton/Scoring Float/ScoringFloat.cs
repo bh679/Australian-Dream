3da67d2920171ebdb443687e1c2e54ca5bf67df9
Brennan Hatton <brennan@brennanhatton.com>
2023-02-27 19:37:44 +1100

    Scoring with static

---
+++ Assets/BrennanHatton/Scoring Float/ScoringFloat.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace BrennanHatton.Scoring
+{
+	
+	public class ScoringFloat : MonoBehaviour
+	{
+		[SerializeField] 
+		protected float value;
+		public bool isStatic;
+		protected static float staticValue;
+		
+		
+		public virtual void UpdateScore(float scoreUpdate)
+		{
+			if(isStatic)
+			{
+				staticValue += scoreUpdate;
+				value = staticValue;
+			}
+			else
+				value += scoreUpdate;
+		}
+		
+		public static virtual void UpdateScore(float scoreUpdate)
+		{
+			staticValue += scoreUpdate;
+		}
+		
+		public virtual void UpdateScoreDeltaTime(float scoreUpdate)
+		{
+			UpdateScore(scoreUpdate*Time.deltaTime);
+		}
+		
+		public virtual void SetScore(float newScore)
+		{
+			if(isStatic)
+			{
+				staticValue = newScore;
+				value = staticValue;
+			}
+			else
+				value = newScore;
+		}
+		
+		public virtual float GetScore()
+		{
+			if(isStatic)
+			{
+				return staticValue;
+			}
+			else
+				return value;
+		}
+	}
+}
---
+++ Assets/BrennanHatton/Scoring Float/UpdateScoreFloat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.Scoring
+{
+	
+	public class UpdateScoreFloat : MonoBehaviour
+	{
+		public ScoringFloat scoreFloat;
+		
+		public float addAmount;
+		
+		public bool updateStatic;
+		
+		[Tooltip("Changes score value by 'addAmount' every second")]
+		public bool OnUpdate;
+	
+	    // Update is called once per frame
+	    void Update()
+	    {
+		    if(OnUpdate)
+			    AddToScore(addAmount);
+	    }
+	    
+		public void AddToScore(float value)
+		{
+			if(updateStatic)
+				ScoringFloat.UpdateScore(value);
+				
+			scoreFloat.UpdateScore(value);
+		}
+	}
+}
