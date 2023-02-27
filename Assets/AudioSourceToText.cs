b3fbbd996c08b583e1e353422e4638cea929025e
Brennan Hatton <brennan@brennanhatton.com>
2023-02-27 18:39:49 +1100

    Prefabing everything

---
+++ Assets/AudioSourceToText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace BrennanHatton.UnityTools
+{
+
+	public class AudioSourceToText : MonoBehaviour
+	{
+		public TMP_Text text;
+		public AudioSource source;
+		
+	    // Start is called before the first frame update
+	    void Start()
+	    {
+	        
+	    }
+	
+	    // Update is called once per frame
+	    void Update()
+	    {
+	        
+	    }
+	}
+}
