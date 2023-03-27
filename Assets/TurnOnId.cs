75ec36d936b8ac6b29344fd406ab87d6e01ce210
Brennan Hatton <brennan@brennanhatton.com>
2023-03-27 16:59:04 +0800

    Three levels running. Changeed level order 3.0.0

---
+++ Assets/TurnOnId.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TurnOnId : MonoBehaviour
+{
+	public UIDocument uiDocument;
+	public string id;
+	GroupBox groupBox;
+		
+	void Reset(){
+		uiDocument = GetComponent<UIDocument>();
+	}
+	public void OnEnable()
+	{
+		groupBox = uiDocument.rootVisualElement.Q(id) as GroupBox;
+			
+	}
+    
+	public void TurnOn()
+	{
+		groupBox.style.display = DisplayStyle.Flex;
+	}
+    
+	public void TurnOff()
+	{
+		groupBox.style.display = DisplayStyle.None;
+	}
+}
