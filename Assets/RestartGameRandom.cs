7099aae5f3b837a4ac552f73eb0a81c63458b8dc
Brennan Hatton <brennan@brennanhatton.com>
2023-04-19 07:58:45 +0800

    Adding boundaries and fixing colliders

---
+++ Assets/RestartGameRandom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGameRandom : MonoBehaviour
+{
+	public void LoadLevelOrNext(int id){
+		//	SceneManager.LoadScene(id);
+		StartCoroutine(WaitLoadLevel(id + ((Random.Range(0,100)>50)?0:1)));
+	}
+		
+	IEnumerator WaitLoadLevel(int x)
+	{
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(x);
+	}
+}
